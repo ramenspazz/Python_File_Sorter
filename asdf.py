6d05d8ee963f9f515c1328b5da20846674aaa3bc
bytewicked <55121859+bytewicked@users.noreply.github.com>
2019-09-27 03:42:53 -0700

    Add files via upload

--- asdf.py
+++ asdf.py
@@ -1,13 +1,32 @@
-import androidhelper
-droid = androidhelper.Android()
-droid.makeToast('Hello, Android!')
-print('\n\nHello, waiting for more...\n')
+import os 
 
+
+
+Audio_EXT = ['.aif', '.cda', '.mid', '.midi', '.mp3', '.mpa', '.ogg',
+             '.wav', '.mwa', '.wpl'],
+
+Documents_EXT = ['.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.txt',
+                 '.wks', '.wps', '.wpd', '.ods', '.xlr', '.xls', '.xlsx', '.key', '.odp',
+                 '.pps', '.ppt', '.pptx', '.csv', '.dat', '.db', '.dbf', '.log', '.mdb',
+                 '.sav', '.sql', '.tar', '.xml', '.html'],
+
+Picture_EXT = ['.ai', '.bmp', '.gif', '.ico', '.jpeg', '.jpg', '.png',
+               '.ps', '.psd', '.svg', '.tif', '.tiff'],
+
+Video_EXT = ['.3g2', '.3gp', '.avi', '.flv', '.h264', '.m4v', '.mkv',
+             '.mov', '.mp4', '.mpg', '.mpeg', '.rm', '.swf', '.vob', '.wmv'],
+
+Program_EXT = ['.apk', '.bat', '.bin', '.cgi', '.pl', '.com', '.exe',
+               '.gadget', '.jar', '.py', '.wsf', '.iso']
+
+    
 
 class sorting_hat:
     def __init__(self, foo = None, slist = None):
+        self.flist = None 
         self.sort_mtd = foo
         self.sort_list = slist
+        self.f_arr = None
     def __iter__(self):
         self.n = 0
         return(self)
@@ -18,22 +37,47 @@ class sorting_hat:
             print("please give function first!\n")
             raise StopIteration
         for j in range(0,len(self.sort_list)):
-            for i in range(0,len(self.sort_list)-j):
+            for i in range(j,len(self.sort_list)):
                 if len(self.sort_list) == i + j + 1:
                     break         
-                if self.sort_list[i+j] > self.sort_list[i+j+1]:
+                if i == j:
+                    continue
+                if self.sort_mtd(self.sort_list[i+j], self.sort_list[i+j+1]):
+                    print("{:d}, {:d}".format(i,j))
                     self.n = self.n + 1
                     temp = self.sort_list[i+j]
                     self.sort_list[i+j] = self.sort_list[i+j+1]
                     self.sort_list[i+j+1] = temp
-                    return(i)
+                    return()
                         #swap 2 elements
         raise StopIteration
-        
-                        
-Harry = sorting_hat(foo = None, slist = [2,5,7,3,9,3,7,1,5,4,7])
+
+    def browse_dir(self):
+        usr_in = None
+        while usr_in != 'q':
+            if usr_in == 'l':
+                self.sort_list = os.listdir()
+            elif not (usr_in == None) and not (usr_in == 'q'):
+                os.chdir(usr_in) 
+            print(os.getcwd())
+            usr_in = input("Enter a direrctory \n ") 
+
+    def sort_file_type(self):
+        self.sort_mtd = lambda a,b: (a.split("."))[-1][0] > (b.split("."))[-1][0]
+
+
+Harry = sorting_hat(foo = lambda a, b : a[0] > b[0], slist = ['zharry','jon','linda', 'barak obama', 'jesus' ,'matthew piazza'])
 
 for dick in Harry:
-    print(dick)
+    pass
     
 Harry. p_list()
+
+Harry.browse_dir()
+
+Harry.sort_file_type()
+
+for needle in Harry:
+    pass
+
+Harry.p_list()
